ea82adbe561dad818c830c54b9a193b5fd95a22a
rhi-teetzel <rhiannateetzel@gmail.com>
2020-03-06 15:34:21 -0500

    getting loading error in jasmine but program runs fine, tried to fix, cannot fix, going to ignore

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -7,137 +7,137 @@
 //"expect"
 //expect(functionWeAreTesting(params))
 
-describe('Cookie Counter Game', function(){
+describe('Cookie Counter Game', function () {
 
     let sut;
 
-        beforeEach(function(){
-            sut = new ClickCounter();
-            this.clickCount = 0;
+    beforeEach(function () {
+        sut = new ClickCounter();
+        this.clickCount = 0;
+    })
+
+    describe('handleClick', function () {
+        it('should record 1 for 1 click', function () {
+            sut.handleClick();
+            expect(sut.clickCount).toBe(1);
         })
 
-        describe('handleClick', function(){
-            it('should record 1 for 1 click', function(){
-                sut.handleClick();
+        it('should return 1 for 1 click', function () {
+            sut.handleClick();
+            expect(sut.getClickCount()).toBe(1);
+        })
+    })
+
+    describe('Click Companion', function () {
+
+        describe('addCompanion', function () {
+            it('should be able to add a companion', function () {
+                sut.clickCount = 100;
+                sut.addCompanion();
+                expect(sut.companionCount).toBe(1);
+            })
+
+            it('should deduct cost of companion from clickCount', function () {
+                sut.clickCount = 100;
+                sut.addCompanion();
+                expect(sut.clickCount).toBe(0);
+            })
+
+
+            it('should increase cost of companion by 10% each time one is purchased', function () {
+                sut.clickCount = 100;
+                sut.addCompanion();
+                expect(sut.companionCost).toBe(110)
+            })
+
+            it('should not allow user to buy companion without enough clicks', function () {
+                sut.clickCount = 90;
+                sut.addCompanion();
+                expect(sut.companionCount).toBe(0)
+            })
+        })
+
+        describe('getCompanionCount', function () {
+            it('should return number of companions', function () {
+                sut.clickCount = 100;
+                sut.addCompanion();
+                expect(sut.getCompanionCount()).toBe(1);
+            })
+        })
+
+        describe('increaseCompanionCost', function () {
+            it('should increase companion cost by 10%', function () {
+                sut.clickCount = 100;
+                sut.increaseCompanionCost();
+                expect(sut.companionCost).toBe(110)
+
+            })
+        })
+
+        describe('addCompanionCountToClickCount', function () {
+            it('should add companion count to click count', function () {
+                sut.clickCount = 100;
+                sut.addCompanion();
+                sut.addCompanionCountToClickCount();
                 expect(sut.clickCount).toBe(1);
             })
 
-            it('should return 1 for 1 click', function(){
-                sut.handleClick();
-                expect(sut.getClickCount()).toBe(1);
+            it('should add companion count to click value for total click count', function () {
+                sut.clickCount = 110;
+                sut.addCompanion();
+                sut.addCompounder();
+                sut.addCompanionCountToClickCount();
+                expect(sut.clickValue).toBe(1.2);
+            })
+        })
+    })
+
+    describe('CollectiveCulminationCompounder', function () {
+
+        describe('addCompounder', function () {
+            it('should add 1 compounder to compounderNumber', function () {
+                sut.clickCount = 100;
+                sut.addCompounder();
+                expect(sut.compounderNumber).toBe(1);
+            })
+
+            it('should deduct 10 from clickcount', function () {
+                sut.clickCount = 100;
+                sut.addCompounder();
+                expect(sut.clickCount).toBe(90);
+            })
+
+            it('add compounder should increase compounderCost by 10%', function () {
+                sut.clickCount = 100;
+                sut.addCompounder();
+                expect(sut.compounderCost).toBe(11);
+            })
+
+            it('should not allow user to buy compounder without 10 clickcount', function () {
+                sut.clickCount = 9;
+                sut.addCompounder();
+                expect(sut.compounderNumber).toBe(0);
+
             })
         })
 
-        describe('Click Companion', function(){
-
-            describe('addCompanion', function(){
-                it('should be able to add a companion', function(){
-                    sut.clickCount = 100;
-                    sut.addCompanion();
-                    expect(sut.companionCount).toBe(1);
-                })
-
-                it('should deduct cost of companion from clickCount', function(){
-                    sut.clickCount = 100;
-                    sut.addCompanion();
-                    expect(sut.clickCount).toBe(0);
-                })
-
-                
-                it('should increase cost of companion by 10% each time one is purchased', function(){
-                    sut.clickCount = 100;
-                    sut.addCompanion();
-                    expect(sut.companionCost).toBe(110)
-                })
-
-                it('should not allow user to buy companion without enough clicks', function(){
-                    sut.clickCount = 90;
-                    sut.addCompanion();
-                    expect(sut.companionCount).toBe(0)
-                })
-            })
-
-            describe('getCompanionCount', function(){
-                it('should return number of companions', function(){
-                    sut.clickCount = 100;
-                    sut.addCompanion();
-                    expect(sut.getCompanionCount()).toBe(1);
-                })
-            })
-
-            describe('increaseCompanionCost', function(){
-                it('should increase companion cost by 10%', function(){
-                    sut.clickCount = 100;
-                    sut.increaseCompanionCost();
-                    expect(sut.companionCost).toBe(110)
-
-                })
-            })
-
-            describe('addCompanionCountToClickCount', function(){
-                it('should add companion count to click count', function(){
-                    sut.clickCount = 100;
-                    sut.addCompanion();
-                    sut.addCompanionCountToClickCount();
-                    expect(sut.clickCount).toBe(1);
-                })
-
-                it('should add companion count to click value for total click count', function(){
-                    sut.clickCount = 110;
-                    sut.addCompanion();
-                    sut.addCompounder();
-                    sut.addCompanionCountToClickCount();
-                    expect(sut.clickValue).toBe(1.2);
-                })
+        describe('getCompounderNumber', function () {
+            it('should return number of compounders', function () {
+                sut.clickCount = 100;
+                sut.addCompounder();
+                expect(sut.getCompounderNumber()).toBe(1);
             })
         })
 
-        describe('CollectiveCulminationCompounder', function(){
-            
-            describe('addCompounder', function(){
-                it('should add 1 compounder to compounderNumber', function(){
-                    sut.clickCount = 100;
-                    sut.addCompounder();
-                    expect(sut.compounderNumber).toBe(1);
-                })
-
-                it('should deduct 10 from clickcount', function(){
-                    sut.clickCount = 100;
-                    sut.addCompounder();
-                    expect(sut.clickCount).toBe(90);
-                })
-
-                it('add compounder should increase compounderCost by 10%', function(){
-                    sut.clickCount = 100;
-                    sut.addCompounder();
-                    expect(sut.compounderCost).toBe(11);
-                })
-
-                it('should not allow user to buy compounder without 10 clickcount', function(){
-                    sut.clickCount = 9;
-                    sut.addCompounder();
-                    expect(sut.compounderNumber).toBe(0);
-
-                })
-            })          
-
-            describe('getCompounderNumber', function(){
-                it('should return number of compounders', function(){
-                    sut.clickCount = 100;
-                    sut.addCompounder();                    
-                    expect(sut.getCompounderNumber()).toBe(1);
-                })
-            })
-
-            describe('increaseClickValue', function(){
-                it('should increase click value by 20%', function(){
-                    sut.clickCount = 1;
-                    sut.increaseClickValue();
-                    expect(sut.clickValue).toBe(1.2);
-                })
+        describe('increaseClickValue', function () {
+            it('should increase click value by 20%', function () {
+                sut.clickCount = 1;
+                sut.increaseClickValue();
+                expect(sut.clickValue).toBe(1.2);
             })
         })
-    
+    })
+
 });
 
 describe('app.js manipulates DOM', () => {
@@ -157,16 +157,27 @@ describe('app.js manipulates DOM', () => {
         testCompanionCount = document.createElement('div');
         testCompounderBuy = document.createElement('button');
         testCompounderCount = document.createElement('div');
-})
-    describe('main click functions', ()=>{
-        it('should show 0 in testClickCount element before any clicks are performed',()=>{
+        makeClickButton(testClickHandler, testClickCount, testClickCounter);
+    })
+    describe('main click functions', () => {
+        it('should show 0 in testClickCount element before any clicks are performed', () => {
             updateClickCount(testClickCount, testClickCounter);
             expect(testClickCount.innerText).toBe('0');
         })
-        it('should show 1 in testClickCount element after one click is performed', ()=>{
+        it('should show 1 in testClickCount element after one click is performed', () => {
             testClickCounter.handleClick();
             updateClickCount(testClickCount, testClickCounter);
             expect(testClickCount.innerText).toBe('1');
+        })
+        describe('makeClickButton() deploys with each click', () => {
+            it('should update testClickCount after button is clicked', () => {
+                testClickHandler.click();
+                expect(testClickCounter.getClickCount()).toBe(1);
+            })
+            it('should update innerText testClickCount after button is clicked', () => {
+                testClickHandler.click();
+                expect(testClickCount.innerText).toBe('1');
+            })
         })
     })
 });
--- src/js/ClickCounter.js
+++ src/js/ClickCounter.js
@@ -1,46 +1,46 @@
-class ClickCounter{
+class ClickCounter {
 
-    constructor(){
+    constructor() {
         this.clickCount = 0;
         this.clickValue = 1;
         this.companionCount = 0;
         this.companionCost = 100;
         this.compounderNumber = 0;
         this.compounderCost = 10;
-        
+
     }
-    
-    handleClick(){
+
+    handleClick() {
         this.clickCount = this.clickValue + this.clickCount;
-    }    
-    getClickCount(){
+    }
+    getClickCount() {
         return this.clickCount;
     }
 
-    
-    addCompanion(){
-        if(this.clickCount >= this.companionCost){
-            this.clickCount -= this.companionCost; 
-            this.companionCount+= 1;
+
+    addCompanion() {
+        if (this.clickCount >= this.companionCost) {
+            this.clickCount -= this.companionCost;
+            this.companionCount += 1;
             this.increaseCompanionCost();
         }
-    }    
+    }
 
-    getCompanionCount(){
+    getCompanionCount() {
         return this.companionCount;
     }
 
-    increaseCompanionCost(){
+    increaseCompanionCost() {
         this.companionCost += this.companionCost * .1;
     }
 
-    addCompanionCountToClickCount(){
-        this.clickCount = this.clickCount + this.companionCount*this.clickValue;
+    addCompanionCountToClickCount() {
+        this.clickCount = this.clickCount + this.companionCount * this.clickValue;
     }
 
 
-    addCompounder(){
-        if(this.clickCount >= this.compounderCost){
+    addCompounder() {
+        if (this.clickCount >= this.compounderCost) {
             this.clickCount -= this.compounderCost;
             this.compounderNumber += 1;
             this.increaseCompounderCost();
@@ -48,27 +48,33 @@ class ClickCounter{
         }
     }
 
-    getCompounderNumber(){
+    getCompounderNumber() {
         return this.compounderNumber;
     }
 
-    increaseCompounderCost(){
+    increaseCompounderCost() {
         this.compounderCost += this.compounderCost * .1;
     }
-    
-    increaseClickValue(){
+
+    increaseClickValue() {
         this.clickValue = this.clickValue + this.clickValue * .2;
     }
-    
+
 
 }
 
 const appClickCounter = new ClickCounter();
 
-const updateClickCount = (clickCount, appClickCounter)=>{
+const updateClickCount = (clickCount, appClickCounter) => {
     clickCount.innerText = appClickCounter.getClickCount();
 }
 
+const makeClickButton = (cookieButton, clickCount, appClickCounter) => {
+    cookieButton.addEventListener('click', ()=>{
+        appClickCounter.handleClick();
+        updateClickCount(clickCount, appClickCounter);
+    })
+}
 
 const clickCount = document.querySelector('.click-count');
 const cookieButton = document.querySelector('.cookie-button');
@@ -80,6 +86,8 @@ const companionButton = document.querySelector('.companion-button');
 const compounderCount = document.querySelector('.compounder-count');
 const compounderButton = document.querySelector('.compounder-button');
 const compounderCost = document.querySelector('.compounder-cost');
+
+makeClickButton(cookieButton, clickCount, appClickCounter);
 
 
 
