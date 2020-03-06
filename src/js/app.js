9b72b92934425658508ae061692f538ae7395e63
rhi-teetzel <rhiannateetzel@gmail.com>
2020-03-06 10:13:29 -0500

    added some dom elements to app.js

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const cookieButton = document.querySelector('.cookie-button');
+const companionButton = document.querySelector('.companion-button');
+const compounderButton = document.querySelector('.compounder-button');
+const clickCount = document.querySelector('.click-count');
+const companionCost = document.querySelector('.companion-cost');
+const compounderCost = document.querySelector('.compounder-cost');
+const appCookieClicker = new CookieClicker();
