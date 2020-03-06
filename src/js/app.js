eb3b093c3ec8eca2cdb366d7e76e7feb496ab5dd
rhi-teetzel <rhiannateetzel@gmail.com>
2020-03-06 10:16:46 -0500

    more dom elements of iteration 3

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
-const cookieButton = document.querySelector('.cookie-button');
-const companionButton = document.querySelector('.companion-button');
-const compounderButton = document.querySelector('.compounder-button');
 const clickCount = document.querySelector('.click-count');
+const cookieButton = document.querySelector('.cookie-button');
+
+const companionCount = document.querySelector('.companion-count');
 const companionCost = document.querySelector('.companion-cost');
+const companionButton = document.querySelector('.companion-button');
+
+const compounderCount = document.querySelector('.compounder-count');
+const compounderButton = document.querySelector('.compounder-button');
 const compounderCost = document.querySelector('.compounder-cost');
+
 const appCookieClicker = new CookieClicker();
