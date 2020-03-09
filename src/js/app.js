9cd2881ccc246e819f6594a789dbf94c99209061
rhi-teetzel <rhiannateetzel@gmail.com>
2020-03-08 21:58:47 -0400

    styling

--- src/js/app.js
+++ src/js/app.js
@@ -105,16 +105,16 @@ function enableCompanionButton() {
 const appClickCounter = new ClickCounter();
 const aboutMe = document.getElementById("about-Me");
 const aboutCompany = document.getElementById("about-Company");
-const resetButton = document.querySelector('.reset-button');
+const resetButton = document.querySelector('#reset-button');
 const clickCount = document.querySelector('.click-count');
 const cookieButton = document.querySelector('.cookie-button');
 
 const companionCount = document.querySelector('.companion-count');
 const companionCost = document.querySelector('.companion-cost');
-const companionButton = document.querySelector('.companion-button');
+const companionButton = document.querySelector('#companion-button');
 
 const compounderCount = document.querySelector('.compounder-count');
-const compounderButton = document.querySelector('.compounder-button');
+const compounderButton = document.querySelector('#compounder-button');
 const compounderCost = document.querySelector('.compounder-cost');
 
 
